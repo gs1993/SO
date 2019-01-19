ad0109576f87bdcbbb306a67379b93c5283b8117
gs1993 <grzegorz.sawinski93@gmail.com>
2019-01-19 16:49:39 +0100

    Add SO models

---
+++ SO/Logic/Models/StackOverflow2010Context.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace Logic.Models
+{
+    public partial class StackOverflow2010Context : DbContext
+    {
+        public StackOverflow2010Context()
+        {
+        }
+
+        public StackOverflow2010Context(DbContextOptions<StackOverflow2010Context> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Badges> Badges { get; set; }
+        public virtual DbSet<Comments> Comments { get; set; }
+        public virtual DbSet<LinkTypes> LinkTypes { get; set; }
+        public virtual DbSet<PostLinks> PostLinks { get; set; }
+        public virtual DbSet<PostTypes> PostTypes { get; set; }
+        public virtual DbSet<Posts> Posts { get; set; }
+        public virtual DbSet<Users> Users { get; set; }
+        public virtual DbSet<VoteTypes> VoteTypes { get; set; }
+        public virtual DbSet<Votes> Votes { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Data Source=.;Initial Catalog=StackOverflow2010;Integrated Security=True");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("ProductVersion", "2.2.1-servicing-10028");
+
+            modelBuilder.Entity<Badges>(entity =>
+            {
+                entity.Property(e => e.Date).HasColumnType("datetime");
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(40);
+            });
+
+            modelBuilder.Entity<Comments>(entity =>
+            {
+                entity.Property(e => e.CreationDate).HasColumnType("datetime");
+
+                entity.Property(e => e.Text)
+                    .IsRequired()
+                    .HasMaxLength(700);
+            });
+
+            modelBuilder.Entity<LinkTypes>(entity =>
+            {
+                entity.Property(e => e.Type)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<PostLinks>(entity =>
+            {
+                entity.Property(e => e.CreationDate).HasColumnType("datetime");
+            });
+
+            modelBuilder.Entity<PostTypes>(entity =>
+            {
+                entity.Property(e => e.Type)
+                    .IsRequired()
+                    .HasMaxLength(50);
+            });
+
+            modelBuilder.Entity<Posts>(entity =>
+            {
+                entity.Property(e => e.Body).IsRequired();
+
+                entity.Property(e => e.ClosedDate).HasColumnType("datetime");
+
+                entity.Property(e => e.CommunityOwnedDate).HasColumnType("datetime");
+
+                entity.Property(e => e.CreationDate).HasColumnType("datetime");
+
+                entity.Property(e => e.LastActivityDate).HasColumnType("datetime");
+
+                entity.Property(e => e.LastEditDate).HasColumnType("datetime");
+
+                entity.Property(e => e.LastEditorDisplayName).HasMaxLength(40);
+
+                entity.Property(e => e.Tags).HasMaxLength(150);
+
+                entity.Property(e => e.Title).HasMaxLength(250);
+            });
+
+            modelBuilder.Entity<Users>(entity =>
+            {
+                entity.Property(e => e.CreationDate).HasColumnType("datetime");
+
+                entity.Property(e => e.DisplayName)
+                    .IsRequired()
+                    .HasMaxLength(40);
+
+                entity.Property(e => e.EmailHash).HasMaxLength(40);
+
+                entity.Property(e => e.LastAccessDate).HasColumnType("datetime");
+
+                entity.Property(e => e.Location).HasMaxLength(100);
+
+                entity.Property(e => e.WebsiteUrl).HasMaxLength(200);
+            });
+
+            modelBuilder.Entity<VoteTypes>(entity =>
+            {
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<Votes>(entity =>
+            {
+                entity.Property(e => e.CreationDate).HasColumnType("datetime");
+            });
+        }
+    }
+}
