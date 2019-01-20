0c830aae74e481a84e3521f452a7db948672598b
gs1993 <grzegorz.sawinski93@gmail.com>
2019-01-20 19:11:35 +0100

    Setup UI project

---
+++ SO/AdminPanel/Pages/Posts/PostsList.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace AdminPanel.Pages.Posts
+{
+    public partial class PostsList : UserControl
+    {
+        public PostsList()
+        {
+            InitializeComponent();
+        }
+    }
+}
