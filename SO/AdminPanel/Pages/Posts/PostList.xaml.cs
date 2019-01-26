b7e01ce4542dff6d973b33c0c2a60aa43ab17dab
gs1993 <grzegorz.sawinski93@gmail.com>
2019-01-26 11:01:07 +0100

    Add PostList Data grid

---
+++ SO/AdminPanel/Pages/Posts/PostList.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading;
+using System.Windows.Controls;
+
+namespace AdminPanel.Pages.Posts
+{
+    public partial class PostList : UserControl
+    {
+        public PostList()
+        {
+            InitializeComponent();
+
+            DataContext = new PostListViewModel();
+        }
+    }
+}
