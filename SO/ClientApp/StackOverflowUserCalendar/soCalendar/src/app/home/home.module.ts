e5497bd344a1c5911ff5dc26887f9ffe5ff2d759
Grzegorz Sawiński <rl3259@PLL00353>
2020-11-15 21:16:16 +0100

    Add calendar app template

---
+++ SO/ClientApp/StackOverflowUserCalendar/soCalendar/src/app/home/home.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
+import { FormsModule } from '@angular/forms';
+import { HomePage } from './home.page';
+import { HomePageRoutingModule } from './home-routing.module';
+
+import { NgCalendarModule  } from 'ionic2-calendar';
+import { CalModalPageModule } from '../pages/cal-modal/cal-modal.module';
+ 
+import { registerLocaleData } from '@angular/common';
+import localeDe from '@angular/common/locales/de';
+registerLocaleData(localeDe);
+
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    HomePageRoutingModule,
+    NgCalendarModule,
+    CalModalPageModule,
+  ],
+  declarations: [HomePage]
+})
+export class HomePageModule {}
