a21ebfb6d3a91997ea9d741e0ca5af515f59693c
admin <admin@DESKTOP-HNFM8BH>
2022-10-23 12:36:54 +0200

    Add DatabaseOptions pattern

--- SO/Logic/Utils/Db/DbExtensions.cs
+++ SO/Logic/Utils/Db/DbExtensions.cs
@@ -1,7 +1,9 @@
 ﻿using Logic.BoundedContexts.Posts.Entities;
 using Logic.BoundedContexts.Users.Entities;
+using Logic.Utils.Db.Options;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 
 namespace Logic.Utils.Db
 {
@@ -10,16 +12,35 @@ namespace Logic.Utils.Db
         public static void AddDbContexts(this IServiceCollection services,
             string commandConnectionString, string queryConnectionString)
         {
-            services.AddDbContext<DatabaseContext>(options => options
-                .UseSqlServer(commandConnectionString,
-                    sqlOptions => sqlOptions.CommandTimeout(180))
-                .UseLazyLoadingProxies()
-            );
+            services.ConfigureOptions<DatabaseOptionsSetup>();
 
-            services.AddDbContext<ReadOnlyDatabaseContext>(options => options
-                .UseSqlServer(queryConnectionString)
-                .UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking)
-            );
+            services.AddDbContext<DatabaseContext>((serviceProvider, optionsBuilder) =>
+            {
+                var databaseOptions = serviceProvider.GetService<IOptions<DatabaseOptions>>()!.Value;
+
+                optionsBuilder
+                    .UseSqlServer(commandConnectionString, sqlOptions => 
+                        sqlOptions.CommandTimeout(databaseOptions.CommandTimeoutInSeconds))
+                    .UseLazyLoadingProxies();
+
+                optionsBuilder.EnableDetailedErrors(databaseOptions.EnableDatailedErrors);
+                optionsBuilder.EnableSensitiveDataLogging(databaseOptions.EnableSensitiveDataLogging);
+            });
+
+            services.AddDbContext<ReadOnlyDatabaseContext>((serviceProvider, optionsBuilder) =>
+            {
+                var databaseOptions = serviceProvider.GetService<IOptions<DatabaseOptions>>()!.Value;
+
+                optionsBuilder
+                    .UseSqlServer(queryConnectionString, sqlOptions =>
+                        sqlOptions.CommandTimeout(databaseOptions.CommandTimeoutInSeconds))
+                    .UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+
+                optionsBuilder.EnableDetailedErrors(databaseOptions.EnableDatailedErrors);
+                optionsBuilder.EnableSensitiveDataLogging(databaseOptions.EnableSensitiveDataLogging);
+            });
+
+
         }
 
         internal static void SetupEntites(ModelBuilder modelBuilder)
---
+++ SO/Logic/Utils/Db/Options/DatabaseOptionsSetup.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+
+namespace Logic.Utils.Db.Options
+{
+    public class DatabaseOptionsSetup : IConfigureOptions<DatabaseOptions>
+    {
+        private const string ConfigurationSectionName = "DatabaseOptions";
+
+        private readonly IConfiguration _configuration;
+
+        public DatabaseOptionsSetup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public void Configure(DatabaseOptions options)
+        {
+            _configuration.GetSection(ConfigurationSectionName).Bind(options);
+        }
+    }
+}
