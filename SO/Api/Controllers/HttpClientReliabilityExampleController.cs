306280f47ac816223ab41f577cd0f4d86dffdae0
admin <admin@DESKTOP-HNFM8BH>
2023-09-16 14:42:23 +0200

    WIP Add http client retry with exponensial backoff and jitter

---
+++ SO/Api/Controllers/HttpClientReliabilityExampleController.cs
@@ -0,0 +1,36 @@
+﻿using Api.Args.Post;
+using Api.Utils;
+using FluentValidation;
+using Logic.Queries.Posts.Dtos;
+using Logic.Read.Posts.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Api.Controllers
+{
+    public class HttpClientReliabilityExampleController : BaseController
+    {
+        public HttpClientReliabilityExampleController(IMediator mediator, IValidatorFactory validationFactory) : base(mediator, validationFactory)
+        {
+        }
+
+        [HttpGet]
+        [SwaggerResponse((int)HttpStatusCode.OK, type: typeof(string))]
+        [SwaggerResponse((int)HttpStatusCode.BadRequest, type: typeof(EnvelopeError))]
+        [SwaggerResponse((int)HttpStatusCode.InternalServerError, type: typeof(EnvelopeError))]
+        public async Task<IActionResult> Get([FromQuery] string query)
+        {
+            
+
+
+            return FromCustomResult(query);
+        }
+    }
+}
