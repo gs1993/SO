a48df2572933388d123b75b82425051b4d59c607
admin <admin@DESKTOP-HNFM8BH>
2023-10-07 23:28:54 +0200

    Test score script

--- .github/scripts/update_badge.py
+++ .github/scripts/update_badge.py
@@ -1,35 +1,17 @@
+import glob
 import json
-import re
-import os
-from github import Github
 
-def get_mutation_score_from_report(report_path):
-    with open(report_path, 'r') as f:
-        report_json = json.load(f)
-    return report_json['mutationScore']
+# Search for mutation-report.json in directories under a specific path
+report_files = glob.glob('/home/runner/work/SO/SO/SO/Tests/UnitTests/StrykerOutput/*/reports/mutation-report.json')
 
-def update_readme_badge(new_score, readme_path):
-    with open(readme_path, 'r') as f:
-        readme_contents = f.read()
+if report_files:
+    report_file = report_files[0]
     
-    updated_contents = re.sub(
-        r'\!\[Mutation Score\]\(https://img.shields.io/badge/Mutation%20Score-\d+\.?\d*%25-[a-zA-Z]+\.svg\)',
-        f'![Mutation Score](https://img.shields.io/badge/Mutation%20Score-{new_score}%25-red.svg)',
-        readme_contents
-    )
-
-    with open(readme_path, 'w') as f:
-        f.write(updated_contents)
-
-if __name__ == '__main__':
-    report_path = './path/to/your/stryker/report.json'
-    readme_path = './README.md'
-    new_score = get_mutation_score_from_report(report_path)
-    
-    update_readme_badge(new_score, readme_path)
-    
-    # Commit and push changes to README
-    g = Github(os.environ['GITHUB_TOKEN'])
-    repo = g.get_repo(os.environ['GITHUB_REPOSITORY'])
-    contents = repo.get_contents("README.md", ref="master")
-    repo.update_file(contents.path, "Update mutation score badge", updated_contents, contents.sha, branch="master")
+    with open(report_file, 'r') as f:
+        data = json.load(f)
+        
+    mutation_score = data.get('mutationScore', None)
+    if mutation_score is not None:
+        print(f'Mutation score is {mutation_score}')
+else:
+    print("No report files found.")
