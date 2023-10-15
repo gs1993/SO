ad4c2fab14c0c6091814b08e01c3641a1298a0b8
admin <admin@DESKTOP-HNFM8BH>
2023-10-15 22:33:27 +0200

    Fix mutation score

--- SO/Tests/UnitTests/Logic/Posts/PostTests.cs
+++ SO/Tests/UnitTests/Logic/Posts/PostTests.cs
@@ -1,5 +1,7 @@
 ﻿using Logic.BoundedContexts.Posts.Entities;
 using Logic.BoundedContexts.Users.Entities;
+using Logic.Utils;
+using System.Text;
 using Xunit;
 
 #pragma warning disable CS8604 // Possible null reference argument.
@@ -16,7 +18,7 @@ namespace UnitTests.Logic.Posts
         {
             _user = User.Create("test", new DateTime(2022, 01, 19), ProfileInfo.Create(null, null, null, null).Value).Value;
             _sut = Post.Create("Test title", "test loooooooooooooong booooodyyyyyyyyyyyyyyyyyyyyyy",
-                new DateTime(2022, 01, 19), 1, "Test User", null).Value;
+                new DateTime(2022, 01, 19), _user, null).Value;
         }
 
         [Fact]
@@ -51,10 +53,9 @@ namespace UnitTests.Logic.Posts
         [Fact]
         public void Should_UpVoteFail_WhenUserIsNull()
         {
-            var result = _sut.UpVote(null);
+            var exception = Assert.Throws<ArgumentNullException>(() => _sut.UpVote(null));
 
-            Assert.NotNull(result.Error);
-            Assert.Equal("user is required", result.Error);
+            Assert.Equal("Value cannot be null. (Parameter 'user')", exception.Message);
             Assert.Equal(0, _sut.Score);
             Assert.Empty(_sut.Votes);
         }
@@ -62,23 +63,98 @@ namespace UnitTests.Logic.Posts
         [Fact]
         public void Should_DownVoteFail_WhenUserIsNull()
         {
-            var result = _sut.DownVote(null);
+            var exception = Assert.Throws<ArgumentNullException>(() => _sut.DownVote(null));
 
-            Assert.NotNull(result.Error);
-            Assert.Equal("user is required", result.Error);
+            Assert.Equal("Value cannot be null. (Parameter 'user')", exception.Message);
             Assert.Equal(0, _sut.Score);
             Assert.Empty(_sut.Votes);
         }
 
         [Fact]
-        public void Should_AddCommentFail_WhenUserIsNull()
+        public void Should_AddCommentThrowAnException_WhenUserIsNull()
         {
-            var result = _sut.AddComment(null, "Test comment");
+            var exception = Assert.Throws<ArgumentNullException>(() => _sut.AddComment(null, "Test comment"));
 
-            Assert.NotNull(result.Error);
-            Assert.Equal("user is required", result.Error);
+            Assert.Equal("Value cannot be null. (Parameter 'user')", exception.Message);
             Assert.Equal(0, _sut.Score);
             Assert.Empty(_sut.Votes);
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        [InlineData("    ")]
+        public void Should_AddCommentFail_WhenCommentIsInvalid(string comment)
+        {
+            var result = _sut.AddComment(_user, comment);
+
+            Assert.True(result.IsFailure);
+            Assert.Equal("Comment cannot be empty", result.Error);
+        }
+
+        [Fact]
+        public void Should_AddCommentFail_WhenCommentIsTooLong()
+        {
+            var result = _sut.AddComment(_user, CreateString(10_001));
+
+            Assert.True(result.IsFailure);
+            Assert.Equal("comment length is invalid", result.Error);
+        }
+
+        [Fact]
+        public void Should_AddCommentSucceed_WhenCommentIsMinLength()
+        {
+            var result = _sut.AddComment(_user, "1");
+
+            Assert.True(result.IsSuccess);
+            var addedComment = Assert.Single(_sut.Comments);
+            Assert.Equal("1", addedComment.Text);
+            Assert.Equal(0, addedComment.Score);
+        }
+
+        [Fact]
+        public void Should_AddCommentSucceed_WhenCommentIsMaxLength()
+        {
+            var comment = CreateString(10_000);
+            var result = _sut.AddComment(_user, comment);
+
+            Assert.True(result.IsSuccess);
+            var addedComment = Assert.Single(_sut.Comments);
+            Assert.Equal(comment, addedComment.Text);
+            Assert.Equal(0, addedComment.Score);
+        }
+
+        [Fact]
+        public void Should_CreateSucceed_WhenAllPropertiesMinLength()
+        {
+            var result = Post.Create
+           (
+               title: CreateString(5),
+               body: CreateString(50),
+               tags: "",
+               createDate: _sut.CreateDate,
+               author: _user
+           );
+
+            Assert.True(result.IsSuccess);
+            Assert.NotNull(result.Value);
+        }
+
+        [Fact]
+        public void Should_CreateSucceed_WhenAllPropertiesMaxLength()
+        {
+            var result = Post.Create
+           (
+               title: CreateString(250),
+               body: CreateString(1000),
+               tags: CreateString(100),
+               createDate: _sut.CreateDate,
+               author: _user
+           );
+
+            Assert.True(result.IsSuccess);
+            Assert.NotNull(result.Value);
         }
 
         [Theory]
@@ -94,8 +170,7 @@ namespace UnitTests.Logic.Posts
                 title: invalidTitle,
                 body: _sut.Body,
                 createDate: _sut.CreateDate,
-                authorId: _user.Id,
-                authorName: _user.DisplayName,
+                author: _user,
                 tags: _sut.Tags
             );
 
@@ -115,13 +190,48 @@ namespace UnitTests.Logic.Posts
                 title: _sut.Title,
                 body: invalidBody,
                 createDate: _sut.CreateDate,
-                authorId: _user.Id,
-                authorName: _user.DisplayName,
+                author: _user,
                 tags: _sut.Tags
             );
 
             Assert.True(result.IsFailure);
             Assert.Contains("body", result.Error.Message);
+        }
+
+        [Fact]
+        public void Should_CloseSucceed_WhenCloseDateIsValid()
+        {
+            var closeDate = new DateTime(2023, 10, 15);
+
+            var result = _sut.Close(closeDate);
+
+            Assert.True(result.IsSuccess);
+            Assert.True(_sut.IsDeleted);
+            Assert.Equal(closeDate, _sut.ClosedDate);
+        }
+
+        [Fact]
+        public void Should_CloseFail_WhenPostWasAlreadyClosed()
+        {
+            var closeDate = new DateTime(2023, 10, 15);
+            _sut.Close(closeDate);
+
+            var result = _sut.Close(closeDate);
+
+            Assert.True(result.IsFailure);
+            Assert.Equal("Post already closed", result.Error);
+        }
+
+
+        private static string CreateString(int length)
+        {
+            StringBuilder sb = new StringBuilder(length);
+            for (int i = 0; i < length; i++)
+            {
+                // Append a random letter (e.g., 'A') or any character you want
+                sb.Append('A');
+            }
+            return sb.ToString();
         }
     }
 }
